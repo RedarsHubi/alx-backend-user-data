ba760065a3b9843b2244c46293a3f7c5dfc47b61
RedarsHubi <redamasters@gmail.com>
2024-05-30 12:30:22 +0100

    annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """Obfuscation function"""
     return re.sub(r'(' + '|'.join(map(re.escape, fields)) +
                   r')=[^{}]+'.format(re.escape(separator)),
                   r'\1=' + redaction,
