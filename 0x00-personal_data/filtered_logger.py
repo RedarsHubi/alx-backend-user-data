90ed4836b74507a3d22150665e86c513cd916b48
RedarsHubi <redamasters@gmail.com>
2024-05-30 12:28:44 +0100

    Obfuscate

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for obfuscation
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(r'(' + '|'.join(map(re.escape, fields)) +
+                  r')=[^{}]+'.format(re.escape(separator)),
+                  r'\1=' + redaction,
+                  message)
