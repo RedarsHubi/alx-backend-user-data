0dbdc812b85e50cbaa869bbf6e883f74baa90e2f
RedarsHubi <redamasters@gmail.com>
2024-06-06 14:49:31 +0100

    8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -37,7 +37,7 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
 
-     def destroy_session(self, request=None):
+    def destroy_session(self, request=None):
         """Deletes de user session / logout"""
 
         if request is None:
