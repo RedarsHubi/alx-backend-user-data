e233f1ed2f9e1e0e9ac6df206c1f38cfbcd5e80d
RedarsHubi <redamasters@gmail.com>
2024-06-13 15:39:01 +0100

    1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""User module"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -6,6 +7,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    """User class"""
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
